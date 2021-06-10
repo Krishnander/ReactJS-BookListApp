16d2489b38c18bc6c7a795c03c9a1c3a2c4bc842
Krishnander <gopi20315@gmail.com>
2021-06-10 22:45:11 +0530

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function BookList(){
+  return(
+  <section>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+  </section>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = ()=>{
+  return (
+    <article>
+      <Image/>
+      <Author/>
+      <Title/>
+    </article>
+  );
+}
+
+const Image = ()=>{
+  return (
+    <img src="https://images-na.ssl-images-amazon.com/images/I/6182z76Pg1L._SY291_BO1,204,203,200_QL40_FMwebp_.jpg" alt=""/>
+  );
+}
+
+const Author = () =>{
+  return (
+    <h4>Becky Lowax</h4>
+  );
+}
+
+const Title = ()=>{
+  return (
+    <h1>Best Parks in USA to visit</h1>
+  );
+}
+
+ReactDom.render(<BookList/>,document.getElementById('root'));
